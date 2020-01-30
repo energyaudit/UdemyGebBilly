abb58e5e49a411d244c66b102d0066e33d40b43a
hyangloyaltycom <hyang2@loyalty.com>
2020-01-30 15:35:44 -0500

    change chrome driver to relative path

--- src/main/java/Account/SpockChrome.groovy
+++ src/main/java/Account/SpockChrome.groovy
@@ -3,11 +3,13 @@
 import geb.Browser
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
-System.setProperty("webdriver.chrome.driver", "/QA INSTALL/chromedriver");
+//System.setProperty("webdriver.chrome.driver", "/QA INSTALL/chromedriver");
+System.setProperty("webdriver.chrome.driver", "../../resources/chromedriver");
 //System.setProperty("webdriver.gecko.driver", "/QA INSTALL/geckodriver");
 //def browser=new Browser(driver:new FirefoxDriver())
 def browser=new Browser(driver:new ChromeDriver())
-browser.go("https://sandbox-beta.airmiles.ca/")
+browser.go("http://www.51.ca")
+//browser.go("https://sandbox-beta.airmiles.ca/")
 
 //class SampleTest extends Specification {
 //
