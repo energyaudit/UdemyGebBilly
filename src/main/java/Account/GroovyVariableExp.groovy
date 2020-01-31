fc97b07b6cf8d4cfb5e8a2e623b72652f65efa88
hyangloyaltycom <hyang2@loyalty.com>
2020-01-30 19:12:15 -0500

    add variable exp

---
+++ src/main/java/Account/GroovyVariableExp.groovy
@@ -0,0 +1,12 @@
+package Account
+
+class GroovyVariableExp {
+    static main( args){
+        def a=5
+        println a
+        a="hello"
+        println "def is dynamic typing type coerce from int to string :   "
+        println a
+    }
+
+}
